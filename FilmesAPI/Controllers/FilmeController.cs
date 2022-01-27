a16a6288fe9eb9fc7c8c18988da5fa76b1d17249
Fernando Raphael Keller <fkeller_3@hotmail.com>
2022-01-27 17:18:48 -0300

    v1.1 - Retornando Dados

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,37 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> Filmes = new();
+        private static int Id = 1;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme) 
+        {
+            filme.Id = Id++;
+            Filmes.Add(filme);            
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RetornaFilmes() 
+        {
+            return Filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RetornaFilmesPorId(int id) 
+        {
+            return Filmes.FirstOrDefault(x => x.Id == id);
+        }
+
+    }
+}
